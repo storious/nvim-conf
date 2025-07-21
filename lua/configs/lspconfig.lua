4f2bf53aa861918a6e320e660400579c73b2d595
storious <wujie1907@gmail.com>
2025-07-22 07:17:00 +0800

    feat: add pyright for python file static check

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,6 +3,9 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "gopls", "pylsp" }
 vim.lsp.enable(servers)
 
+-- pyright config
+vim.lsp.config("pyright", {})
+
 -- read :h vim.lsp.config for changing options of lsp servers
 vim.lsp.config("lua_ls", {
   hint = { enable = true },
