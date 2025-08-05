a9b3fdc6c64ecb0d571b51b3d5fdac26266fe096
storious <wujie1907@gmail.com>
2025-08-05 22:17:31 +0800

    feat: add dap mappings for debug

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,19 +8,40 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")--
+-- NOTE: nvim-dap mappings
+map("n", "<leader>dr", function()
+  require("dap").repl.open()
+end, { desc = "Debug open dap reply window" })
 
--- nvim-dap debug map
-map("n", "<leader>td", function()
+map("n", "<leader>sb", function()
+  require("dap").set_breakpoint()
+end, { desc = "Debug set breakpoint" })
+
+map("n", "<leader>tb", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Debug toggle breakpoint" })
+
+map("n", "<leader>dl", function()
+  require("dap").run_last()
+end, { desc = "Test run last test" })
+
+-- NOTE: nvim-dap-go debug mappings
+map("n", "<leader>dt", function()
   require("dap-go").debug_test()
-end, { desc = "debug test go test function" })
+end, { desc = "Debug golang test function" })
 
--- nvim-dap-python map
-map("n", "<leader>dn", function()
+-- NOTE: nvim-dap-python mappings
+map("n", "<leader>pm", function()
   require("dap-python").test_method()
-end, { desc = "py test method" })
+end, { desc = "Test python method" })
 
-map("n", "<leader>df", function()
+map("n", "<leader>pc", function()
   require("dap-python").test_class()
-end, { desc = "py test class" })
+end, { desc = "Test python class" })
 
-map("v", "<leader>ds", '<ESC>lua: require("dap-python").debug_selection()<CR>', { desc = "py debug test selection" })
+map(
+  "v",
+  "<leader>ps",
+  '<ESC>lua: require("dap-python").debug_selection()<CR>',
+  { desc = "Debug python test selection" }
+)
