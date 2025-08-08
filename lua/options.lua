feb62c8b501425c1e5217dfa0228db6dd92a5f62
storious <wujie1907@gmail.com>
2025-08-08 10:32:37 +0800

    chore: use 4 space replace tab

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,11 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- NOTE: set 4 space for tab , shiftwidth and softtab
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+o.expandtab = true
